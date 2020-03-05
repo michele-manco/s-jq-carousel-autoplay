7f9a40f896996c65778e9b4c55ddf12683d4ebf5
Michele_Manco <motlamik@gmail.com>
2020-03-05 19:03:07 +0100

    js fixed

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ $('.menu-items').click(function() {
   if (!$(this).children('.dropd-menu').is(':visible')) {
     $(this).children('.dropd-menu').slideDown()
   }
-});$('.next').click(function(scorri){
+});
+
+
+$('.next').click(function(scorri){
  var img_corrente = $('img.active');
  var dot_corrente = $('i.active');
  var img_successiva = img_corrente.next('img');
@@ -14,7 +17,7 @@ $('.menu-items').click(function() {
  img_corrente.removeClass('active');
  img_successiva.addClass('active');
  if (dot_successivo.length == 0) {
-   dot_successivo = $('.slider i:first-child');
+   dot_successivo = $('.bullett i:first-child');
  };
  dot_corrente.removeClass('active');
  dot_successivo.addClass('active');
@@ -31,27 +34,29 @@ $('.prev').click(function(){
  img_corrente.removeClass('active');
  img_precedente.addClass('active');
  if (dot_precedente.length == 0) {
-   dot_precedente = $('.slider i:last-child');
+   dot_precedente = $('.bullett i:last-child');
   }
  dot_corrente.removeClass('active');
  dot_precedente.addClass('active');
 });
-var clock = setInterval(function scorri() {
-  var img_corrente = $('img.active');
-  var img_successiva = img_corrente.next('img');
-  if (img_successiva.length == 0) {
-    img_successiva = $('.slider img:first-child');
-  }
-  img_corrente.removeClass('active');
-  img_successiva.addClass('active');
-  var dot_corrente = $('i.active');
 
-  var dot_successivo = dot_corrente.next('i');
 
-  if (dot_successivo.length == 0) {
-    dot_successivo = $('.bullett i:first-child');
-  }
-  dot_corrente.removeClass('active');
-  dot_successivo.addClass('active');
+ var clock = setInterval(function scorri() {
+   var img_corrente = $('img.active');
+   var img_successiva = img_corrente.next('img');
+   if (img_successiva.length == 0) {
+     img_successiva = $('.slider img:first-child');
+   }
+   img_corrente.removeClass('active');
+   img_successiva.addClass('active');
+   var dot_corrente = $('i.active');
 
-}, 3000);
+   var dot_successivo = dot_corrente.next('i');
+
+   if (dot_successivo.length == 0) {
+     dot_successivo = $('.bullett i:first-child');
+   }
+   dot_corrente.removeClass('active');
+   dot_successivo.addClass('active');
+
+ }, 3000);
